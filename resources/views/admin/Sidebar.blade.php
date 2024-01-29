42958823ae6417284557e12f187cdd3ab3916108
Limon Islam <limon4u714@gmail.com>
2024-01-29 19:36:09 +0600

    Database added

---
+++ resources/views/admin/Sidebar.blade.php
@@ -0,0 +1,11 @@
+ <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
+ <ul class="list-unstyled">
+    <li class="active"><a href="{{ route('home') }}"> <i class="icon-home"></i>Home </a></li>
+    <li><a href="{{ url('postpage') }}"> <i class="icon-grid"></i>Add Post </a></li>
+    <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
+    <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
+    <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
+</ul><span class="heading">Extras</span>
+
+</nav>
+<!-- Sidebar Navigation end-->
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,22 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\postpageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 Route::get('/home',[HomeController::class,'home'])
 ->middleware('auth')
 ->name('home');
 
-// Route::get('/post',[HomeController::class,'post'])
-// ->middleware('auth','admin')
-// ->name('post');
 
 Route::get('/',[HomeController::class,'homepage']);
+Route::get('/postpage',[postpageController::class,'postpage']);
+Route::post('/add_post',[postpageController::class,'add_post']);
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
